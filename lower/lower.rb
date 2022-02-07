741f8a221032078f9b23f87c87a22880100d6435
Jônatas Davi Paganini <jonatasdp@gmail.com>
2022-02-07 08:35:52 -0300

    Add special random processing

--- lower/lower.rb
+++ lower/lower.rb
@@ -10,6 +10,7 @@ require 'faraday'
 require 'opentelemetry/sdk'
 require 'sinatra/base'
 require 'json'
+require 'net/http'
 
 Bundler.require
 
@@ -77,9 +78,33 @@ set :port, 5000
 
 use OpenTelemetryMiddleware
 CHARS = ('a'..'z').to_a
+def work time, description, c
+  @tracer.in_span(span_name, attributes: { "char" => c}, kind: :server) do |span|
+    sleep time
+  end
+end
+
+def get_digit
+  digit = Net::HTTP.get(URI("http://digit:5000"))
+  json = JSON.parse(digit)
+  json['char']
+end
+
+def prepare_char
+  c = CHARS.sample
+  case c
+  when 'z', 'x', 'r'
+    work(0.05, 'extra_process_lower',  c)
+  when 'z', 'a', 't'
+    work(0.01, 'extra_extra_process_lower',  c)
+  end
+  c
+end
 
 get '/' do
   content_type :json
-  c = CHARS.sample
+
+  c = prepare_char
+
   {char: c}.to_json
 end
