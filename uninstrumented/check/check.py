830afe46d77c6654e86cc3c40077796a35482259
James Sewell <james.sewell@gmail.com>
2022-05-20 12:50:38 +0200

    Inject schema creation on first check pod run

--- uninstrumented/check/check.py
+++ uninstrumented/check/check.py
@@ -6,16 +6,27 @@ from flask import Flask, Response, request
 app = Flask(__name__)
 
 def check_password(digest: str) -> bool:
-    cnx = psycopg2.connect(host='timescaledb',database='otel_demo',user='postgres')
+    cnx = psycopg2.connect()
     cursor = cnx.cursor()
     # This will get an exception if the password has been used
     # as the PRIMARY KEY will clash
+    cursor.execute("create table if not exists used_passwords(digest text primary key);")
     cursor.execute("INSERT INTO used_passwords VALUES (%s)", (digest,))
     cursor.close()
     cnx.commit()
     cnx.close()
     return 'OK'
         
+@app.before_first_request
+def before_first_request():
+    cnx = psycopg2.connect()
+    cursor = cnx.cursor()
+    cursor.execute("create table if not exists used_passwords(digest text primary key);")
+    cursor.close()
+    cnx.commit()
+    cnx.close()
+        
+@app.before_first_request
 
 @app.route('/')
 def check():
@@ -23,4 +34,4 @@ def check():
 
 
 if __name__ == '__main__':
-    app.run()
+   app.run()
